75471c298945d3113ed6f8f9bd3be9d2221755e4
DJ Rodriguez <djayr24@gmail.com>
2018-08-28 21:00:00 -0700

    add subscribe to products on component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ps: ProductsService) { }
+
 
   ngOnInit() {
+    this.ps.getProducts()
+    .subscribe(products => {
+      console.log(products);
+    }) 
   }
 
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private api: ApiService ) { }
+  getProducts(): any {
+    return this.api.get('/products')
+  }
 }
