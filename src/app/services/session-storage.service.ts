90e47f7260cf17d3410ca4e884426a81bc7057e2
DJ Rodriguez <djayr24@gmail.com>
2018-08-28 19:15:05 -0700

    added local storage info to the session storage service

--- src/app/services/session-storage.service.ts
+++ src/app/services/session-storage.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SessionStorageService {
-
+  private app = 'mean.';
+  
   constructor() { }
+  set(key, data) {
+    return localStorage.setItem(`${this.app}${key}`, JSON.stringify(data));
+  };
+  get(key) {
+    return JSON.parse(localStorage.getItem(`${this.app}${key}`)); //this is getting string from the set function and turning it back into an object
+  };
+
+  remove(key) {
+    return localStorage.removeItem(`${this.app}${key}`);
+  }
 }
